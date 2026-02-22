d067874cb6c6d2bf519ba223611c881dfeb55ca0
Daniel <dani.damqnov.dimitrov@gmail.com>
2026-02-22 02:04:44 +0200

    Added implementation for finding sum of elements with same indexes

---
+++ HackerRank/Program.cs
@@ -0,0 +1,24 @@
+﻿// Case 1
+using HackerRank;
+
+var a_indexes = new List<int>
+{
+    1,4,5
+};
+var a_values = new List<double>
+{
+    9,3,1
+};
+
+var b_indexes = new List<int>
+{
+    5,4,4
+};
+var b_values = new List<double>
+{
+    2,1,2
+};
+
+var result = Solution.Calculate(a_indexes, a_values, b_indexes, b_values);  // Expected 6.5
+
+Console.WriteLine(result);
---
+++ HackerRank/Solution.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank;
+
+internal static class Solution
+{
+    internal static double Calculate(
+        List<int> a_indexes,
+        List<double> a_values,
+        List<int> b_indexes,
+        List<double> b_values)
+    {
+        double result = default;
+        var count = a_indexes.Count;
+
+        var aPairsDict = new Dictionary<int, double>();
+        var aIndexesFound = new Dictionary<int, int>();
+
+        var bPairsDict = new Dictionary<int, double>();
+        var bIndexesFound = new Dictionary<int, int>();
+
+        for (int i = 0; i < count; i++)
+        {
+            var aIndex = a_indexes[i];
+            var aValue = a_values[i];
+
+            if (aPairsDict.TryGetValue(aIndex, out var aPairValue))
+            {
+                aPairsDict[aIndex] = aPairValue + aValue;
+
+                if(aIndexesFound.ContainsKey(aIndex))
+                {
+                    aIndexesFound[aIndex] = aIndexesFound[aIndex] + 1;
+                }
+                else
+                {
+                    aIndexesFound[aIndex] = 2;
+                }
+            }
+            else
+            {
+                aPairsDict.Add(aIndex, aValue);
+            }
+
+            var bIndex = b_indexes[i];
+            var bValue = b_values[i];
+
+            if (bPairsDict.TryGetValue(bIndex, out var bPairValue))
+            {
+                bPairsDict[bIndex] = bPairValue + bValue;
+
+                if (bIndexesFound.ContainsKey(bIndex))
+                {
+                    bIndexesFound[bIndex] = bIndexesFound[bIndex] + 1;
+                }
+                else
+                {
+                    bIndexesFound[bIndex] = 2;
+                }
+            }
+            else
+            {
+                bPairsDict.Add(bIndex, bValue);
+            }
+        }
+
+        foreach(var bPair in bPairsDict)
+        {
+            if(aPairsDict.TryGetValue(bPair.Key, out var aTotalValue))
+            {
+                var tempAValue = aTotalValue;
+
+                if(aIndexesFound.TryGetValue(bPair.Key, out var totalFound))
+                {
+                    tempAValue = tempAValue / totalFound;
+                }
+
+                var tempBValue = bPair.Value;
+
+                if (bIndexesFound.TryGetValue(bPair.Key, out var bTotalFound))
+                {
+                    tempBValue = tempBValue / bTotalFound;
+                }
+
+                result = result + (tempAValue * tempBValue);
+            }
+        }
+
+        return result;
+    }
+}
